bec55e91ebb6cdd103bad37b0a3a3f025bf099e9
Victor Schwan <victor.schwan@cern.ch>
2025-03-14 09:42:39 +0100

    remote task runs: fixed law_setup.sh

--- law_setup.sh
+++ law_setup.sh
@@ -1,6 +1,16 @@
 # incorporate in $OC/my.env.sh
-export LAW_HOME=${bsDir}/.law
-export LAW_CONFIG_FILE=${bsDir}/.law
 
-export DATA_PATH="${dtDir}/test_law"
-export ANALYSIS_PATH="${codeDir}/beamStrahlung"
+action() {
+    local shell_is_zsh="$( [ -z "${ZSH_VERSION}" ] && echo "false" || echo "true" )"
+    local this_file="$( ${shell_is_zsh} && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
+    local this_dir="$( cd "$( dirname "${this_file}" )" && pwd )"
+
+    export PYTHONPATH="${this_dir}:${PYTHONPATH}"
+
+    export ANALYSIS_PATH="${codeDir}/beamStrahlung"
+    export LAW_HOME=${ANALYSIS_PATH}/.law
+    export LAW_CONFIG_FILE=${ANALYSIS_PATH}/law.cfg
+
+    export DATA_PATH="${dtDir}/test_law"
+}
+action
