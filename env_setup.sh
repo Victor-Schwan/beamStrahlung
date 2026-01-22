5f37f0ea48f8c44fba037363bdd183b12430d494
Victor Schwan <victor.schwan@cern.ch>
2026-01-22 22:51:47 +0100

    [env_setup] point to k4geo installation instead of source

--- env_setup.sh
+++ env_setup.sh
@@ -3,5 +3,5 @@ source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 
 # assert that all k4geo path vars point to the same place
 echo "set local k4geo"
-export k4geo_DIR="$k4gDir"
+export k4geo_DIR="$k4gDir/install/share/k4geo"
 export K4GEO="$k4geo_DIR"
